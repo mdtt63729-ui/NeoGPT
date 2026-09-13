741255a95e42670ec223ff50daac30d4ba034e5e
Gitofy <gitofy@users.noreply.github.com>
2026-09-13 08:20:48 +0000

    chore(gitofy): replace project from ZIP [skip ci]

--- app/src/main/java/com/neogpt/app/ui/screens/live/LiveConversationScreen.kt
+++ app/src/main/java/com/neogpt/app/ui/screens/live/LiveConversationScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
